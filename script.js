2c149ce77542f0bc79176f1472a970a32b0fea40
Nessa515 <vanessaoliveiradasilva515@gmail.com>
2024-08-12 20:22:59 -0400

    Feito o index

---
+++ script.js
@@ -0,0 +1,28 @@
+const atividade = document.querySelector("#atividade");
+const sala = document.querySelector("#sala");
+const dataInicial = document.querySelector("#data_inicial");
+const dataFinal = document.querySelector("#data_final");
+const btnReservar = document.querySelector("#btnReservar");
+
+const reservas = [];
+
+function addReserva(atividade, sala, dataInicial, dataFinal){
+    const reserva = {
+        atividade:    atividade,
+        sala:         sala,
+        dataInicial:  new Date(dataInicial),
+        dataFinal:    new Date(dataFinal)
+    }
+    reservas.push(reserva);
+    console.table(reserva)
+}
+
+btnReservar.addEventListener('click', (e) => {
+    e.preventDefault()
+    addReserva(
+        atividade.value,
+        sala.value,
+        dataInicial.value,
+        dataFinal.value
+    )
+});

fe0fcd11f6f531e0b8ae62f180f5c1da4770b4eb
Nessa515 <vanessaoliveiradasilva515@gmail.com>
2024-08-15 22:16:55 -0400

    Feito o script e a estilização

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const sala = document.querySelector("#sala");
 const dataInicial = document.querySelector("#data_inicial");
 const dataFinal = document.querySelector("#data_final");
 const btnReservar = document.querySelector("#btnReservar");
+const tabela = document.querySelector("#tabela");
 
 const reservas = [];
 
@@ -14,7 +15,35 @@ function addReserva(atividade, sala, dataInicial, dataFinal){
         dataFinal:    new Date(dataFinal)
     }
     reservas.push(reserva);
-    console.table(reserva)
+    renderizarTabela();
+}
+
+function renderizarTabela(){
+    tabela.innerHTML = `
+        <table class="tabela">
+            <tr>
+                <th>Atividade</th>
+                <th>Sala</th>
+                <th>Data de início</th>
+                <th>Data de fim</th>
+            </tr>
+            ${reservas.map(reserva => 
+                `   
+                <tr>
+                    <td>${reserva.atividade}</td>
+                    <td>${reserva.sala}</td>
+                    <td>${getDataFormatada(reserva.dataInicial)}</td>
+                    <td>${getDataFormatada(reserva.dataFinal)}</td>
+                </tr>
+                `
+            ).join('')}
+        </table>
+    `;
+}
+
+function getDataFormatada(data){
+    return `${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()} - 
+    ${data.getHours()}:${data.getMinutes()}`
 }
 
 btnReservar.addEventListener('click', (e) => {
